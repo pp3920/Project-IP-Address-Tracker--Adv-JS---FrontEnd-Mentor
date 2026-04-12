a307e5d84e63264f923a343037b71080b803b68b
Priyanka22324 <priyanka22324@gmail.com>
2026-04-12 18:55:45 -0400

    adding script file

--- script.js
+++ script.js
@@ -1,72 +1,47 @@
-const input = document.querySelector("input");
-const arrow = document.querySelector("arrow");
-const list = document.querySelectorAll("info");
+const input = document.querySelector(".input");
+const arrow = document.querySelector(".arrow");
+const list = document.querySelectorAll(".info");
 
-let marker = null; // IMPORTANT
+let marker = null;
 
-// Default map (Delhi)
 const map = L.map('map').setView([28.644800, 77.216721], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
    attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
-// Default marker
 marker = L.marker([28.644800, 77.216721]).addTo(map);
 
 function sendRequest() {
     let inputValue = input.value;
-
-    // agar user kuch na dale to default IP use ho
     let ip = inputValue || "8.8.8.8";
 
     fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_zL8x2lD0EwuysaAW3w1jSMm3m0e4L&ipAddress=${ip}`)
-    .then((response) => response.json())
-    .then((responseData) => {
-        //console.log(responseData);
-        if (responseData.code === 422) {
+    .then(res => res.json())
+    .then(data => {
+
+        console.log(data);
+        if (data.code === 422) {
             alert("Invalid IP Address");
             return;
         }
 
-          //  UI update
-        list[0].innerHTML = responseData.ip;
-        list[1].innerHTML =
-            responseData.location.city + ", " +
-            responseData.location.region + " " +
-            responseData.location.country;
+        list[0].innerHTML = data.ip;
+        list[1].innerHTML = `${data.location.city}, ${data.location.region} ${data.location.country}`;
+        list[2].innerHTML = "UTC " + data.location.timezone;
+        list[3].innerHTML = data.isp;
 
-        list[2].innerHTML = "UTC " + responseData.location.timezone;
-        list[3].innerHTML = responseData.isp;
-
-        //Map update
-        const lat = responseData.location.lat;
-        const lng = responseData.location.lng;
+        const { lat, lng } = data.location;
 
         map.setView([lat, lng], 13);
 
-        // remove old marker
-        if (marker !== null) {
-            map.removeLayer(marker);
-        }
+        if (marker) map.removeLayer(marker);
 
-        // add new marker
         marker = L.marker([lat, lng]).addTo(map)
-            .bindPopup(responseData.ip)
+            .bindPopup(data.ip)
             .openPopup();
     })
-    .catch((error) => {
-        console.error("Error:", error);
-    });
+    .catch(err => console.error(err));
 }
 
-
-// Click event
-//arrow.addEventListener("click", sendRequest);
-
-// Enter key event
-// document.addEventListener("keypress", (e) => {
-//     if (e.key === "Enter") {
-//         sendRequest();
-//     }
-// });
+arrow.addEventListener("click", sendRequest);
